4645464925af3cf7ad84e3cc6ad06daf6842273d
Pascale, Michael Philip <mppascale@mgh.harvard.edu>
2022-12-12 12:45:40 -0500

    feat: list_idx, join_on_idx, write_csv

--- R/R00-Utilities_and_operators.R
+++ R/R00-Utilities_and_operators.R
@@ -32,6 +32,10 @@
 #     3.2.3) camr_ymd_hms
 #     3.2.4) camr_hms
 #   3.3) camr_pass
+#   3.4) Naming convention utilities
+#     3.4.1) camr_list_idx
+#     3.4.2) camr_join_on_idx
+#   3.5) camr_write_csv
 
 #### 1) Operators ####
 
@@ -550,4 +554,82 @@ camr_pass <- function(
     warning(.chr_pass_message)
 
   NULL
+}
+
+#### 3.4) Naming Convention Utilities ####
+##### 3.4.1) camr_list_idx #####
+
+#' List CAM index columns of a dataframe.
+#'
+#' By convention, columns beginning with the prefix IDX are index columns.
+#'
+#' @param df_x A dataframe, with columns following the CAM naming convention.
+#'
+#' @return A character vector of the names in df_x prfixed by IDX.
+#' @export
+camr_list_idx <- function(df_x) {
+  df_x |> colnames() |> keep(str_detect, '^IDX\\.')
+}
+
+##### 3.4.2) camr_join_on_idx #####
+#' Safely left-join two dataframes on CAM index columns.
+#'
+#' `r lifecycle::badge("experimental")`
+#'
+#' By convention, columns beginning with the prefix IDX are index columns. This
+#' function will perform a `left_join`, mapping the IDX columns of `df_y` onto
+#' those matching in `df_x`.
+#'
+#' @param df_x A dataframe, with columns following the CAM naming convention.
+#' @param df_y A dataframe, with columns following the CAM naming convention.
+#'
+#' @return left_join(df_x, df_y, by=camr_list_idx(df_y))
+#' @keywords internal
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' df_scans |>
+#'   camr_join_on_idx(
+#'     df_demographics)
+#'   )
+#' }
+camr_join_on_idx <- function (df_x, df_y) {
+  vchr_indicies <- camr_list_idx(df_y)
+
+  if(!all(vchr_indicies%in% colnames(df_x))) {
+    stop(
+      'camr_join_on_idx expects that the IDX columns of df_y will be present in df_x\n',
+      '    df_y has indicies: ', paste(camr_list_idx(df_scans), collapse=', ')
+    )
+  }
+
+  left_join(df_x, df_y, by=vchr_indicies)
+}
+
+
+##### 3.5) camr_write_csv #####
+#' Write a CSV file with the standard options.
+#'
+#' `r lifecycle::badge("experimental")`
+#'
+#' The provided filename will be expanded with `camr_name_file`.
+#'
+#' @param df_x A dataframe to be written.
+#' @param chr_filename A dataframe, with columns following the CAM naming convention.
+#' @param ... Additional arguments to write.csv(),
+#'
+#' @keywords internal
+#' @export
+camr_write_csv <- function(df_x, chr_filename, row.names=FALSE, na='', lgl_literal=FALSE) {
+  assert_data_frame(df_x)
+  assert_string(chr_filename, pattern='\\.csv$')
+  assert_logical(lgl_literal, len=1, any.missing=FALSE)
+
+  if (!lgl_literal)
+    chr_filename <- camr_name_file(chr_filename)
+
+
+  write.csv(df_x, chr_filename, row.names=row.names, na=na, eol='\r\n', fileEncoding='UTF-8')
+  message('Wrote ', deparse(substitute(df_x)), ' to ', chr_filename, '.')
 }
