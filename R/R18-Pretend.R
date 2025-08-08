c30fb0343ba5f5b4ed6665bf941d16a546ace381
Zachary Himmelsbach <zacharyhimmelsbach@zacharys-mbp.partners.org>
2025-08-08 15:20:50 -0400

    Add camr_pretend beta function.

---
+++ R/R18-Pretend.R
@@ -0,0 +1,16 @@
+#' This is a pretend function
+#'
+#' @description
+#' Totally fake; for educational purposes only
+#'
+#' @param v A character vector
+#' @returns Side effect: prints a nice message
+#'
+#' @import meme
+#' @export
+#'
+#'
+camr_pretend <- function(v) {
+  meme("jpeg", "string1", "ahhhh")
+  sprintf("I love this vector: %s", v)
+}
